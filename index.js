f7fa2c55255fd1b6b9a61a11a6d0260c5a01216b
Igor Rakush <irakush@aol.com>
2023-11-13 23:05:16 -0500

    functions lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+const ft = 264;
+
+function distanceFromHqInBlocks(start){
+  return Math.abs(start - 42);
+}
+
+function distanceFromHqInFeet(start) {
+  return Math.abs(start - 42) * ft;
+}
+
+function distanceTravelledInFeet(start, end) {
+  return Math.abs(start - end) * ft;
+}
+
+function calculatesFarePrice(start, end) {
+  const distance = Math.abs(start - end) * ft
+
+  if (distance > 400 && distance <= 2000) {
+    return (distance - 400) * 0.02;
+  }
+
+  if (distance > 2000 && distance <= 2500) {
+    return 25;
+  }
+
+  if (distance > 2500) {
+    return 'cannot travel that far'
+  }
+
+  return 0;
+}
